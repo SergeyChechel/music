9f9e321ee2b69b99e57348ae1fa59a63802e9040
Your Name <lisascol8classssss@gmail.com>
2023-10-15 16:21:56 +0300

    initial commit

---
+++ src/includes/firebase.js
@@ -0,0 +1,25 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore"; // firebase/database в старой редакции
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDVhXvgFggX3_PKP-mDTPg1tnGibRjxVTk",
+    authDomain: "music-2fe52.firebaseapp.com",
+    projectId: "music-2fe52",
+    storageBucket: "music-2fe52.appspot.com",
+    appId: "1:828473617262:web:fb0e2b7a0a114f9fa4af7f"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+const usersCollection = db.collection('users'); 
+
+export {
+  auth,
+  db,
+  usersCollection
+}
