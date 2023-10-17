003e7f325f70b96104116cdc9807961ab0c32eb7
User1 <chechelsk@gmail.com>
2023-10-17 16:43:44 +0300

    workin with routes. protecting routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,50 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from "@/views/Home.vue"
-import About from "@/views/About.vue"
+import Home from "@/views/Home.vue";
+import About from "@/views/About.vue";
+import Manage from "@/views/Manage.vue";
 
 const routes = [
   {
+    name: "home",
     path: "/",
     component: Home,
   },
   {
+    name: "about",
     path: "/about",
     component: About,
+  },
+  {
+    name: "manage",
+    // alias: "/manage",
+    path: "/manage-music",
+    component: Manage,
+    beforeEnter: (to, from, next) => {
+      console.log('Manage Route Guard');
+      next();
+    }
+  },
+  {
+    path: '/manage',
+    redirect: { name: 'manage' },
+  },
+  {
+    path: '/:catchAll(.*)*',
+    redirect: { name: 'home' },
   }
 ];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
-})
+  linkExactActiveClass: 'text-yellow-500',
+});
+
+router.beforeEach((to, from, next) => {
+  console.log('Global gard');
+
+  
+  next();
+});
 
 export default router
