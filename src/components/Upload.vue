d4de8c0da6151987c8cbb43293455b84391df8c6
User1 <chechelsk@gmail.com>
2023-10-23 14:19:43 +0300

    continue working with uploading files

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -26,14 +26,14 @@
               <!-- Progess Bars -->
               <div class="mb-4" v-for="upload in uploads" :key="upload.name">
                 <!-- File Name -->
-                <div class="font-bold text-sm">
-                  {{ upload.name }}
+                <div class="font-bold text-sm" :class="upload.text_class">
+                  <i :class="upload.icon"></i> {{ upload.name }}
                 </div>
                 <div class="flex h-4 overflow-hidden bg-gray-200 rounded">
                   <!-- Inner Progress Bar -->
                   <div
-                    class="transition-all progress-bar bg-blue-400"
-                    :class="'bg-blue-400'"
+                    class="transition-all progress-bar"
+                    :class="upload.variant"
                     :style="{ width: upload.current_progress + '%' }"
                   ></div>
                 </div>
@@ -44,7 +44,7 @@
 </template>
 
 <script>
-import { storage } from '@/includes/firebase';
+import { storage, auth, songsCollection } from '@/includes/firebase';
 
 export default {
   name: 'upload',
@@ -66,14 +66,39 @@ export default {
             const songsRef = storageRef.child(`songs/${file.name}`); // вернет объект, содержащий относительный путь - '/songs/<имя файла>.mp3'
             const task = songsRef.put(file);
 
-            this.uploads.push({
+            const uploadIndex = this.uploads.push({
               task,
               current_progress: 0,
               name: file.name,
-            });
+              variant: 'bg-blue-400',
+              icon: 'fas fa-spinner fa-spin',
+              text_class: '',
+            }) - 1;
 
             task.on('state_changed', (snapshot) => {
                 const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                this.uploads[uploadIndex].current_progress = progress;
+            }, (error) => {
+              this.uploads[uploadIndex].variant = 'bg-red-400';
+              this.uploads[uploadIndex].icon = 'fas fa-times';
+              this.uploads[uploadIndex].text_class = 'text-red-600';
+              console.log(error);              
+            }, async () => {
+              const song = {
+                uid: auth.currentUser.uid,
+                display_name: auth.currentUser.displayName,
+                original_name: task.snapshot.ref.name,
+                modified_name: task.snapshot.ref.name,
+                genre: '',
+                comment_count: 0,
+              };
+
+              song.url = await task.snapshot.ref.getDownloadURL();
+              await songsCollection.add(song);
+
+              this.uploads[uploadIndex].variant = 'bg-green-400';
+              this.uploads[uploadIndex].icon = 'fas fa-check';
+              this.uploads[uploadIndex].text_class = 'text-green-600';
             });
         })
     }
