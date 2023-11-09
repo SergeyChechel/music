89a4b7cdb2f3c575d799a18a4a13a42c3c0a1f72
Sergey <chechelsk@gmail.com>
2023-11-09 14:35:57 +0200

    Passing Values to Directives

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,9 +10,10 @@
         <div class="text-white main-header-content">
           <h1 class="font-bold text-5xl mb-5">Listen to Great Music!</h1>
           <p class="w-full md:w-8/12 mx-auto">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus et dolor mollis,
-            congue augue non, venenatis elit. Nunc justo eros, suscipit ac aliquet imperdiet,
-            venenatis et sapien. Duis sed magna pulvinar, fringilla lorem eget, ullamcorper urna.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+            et dolor mollis, congue augue non, venenatis elit. Nunc justo eros,
+            suscipit ac aliquet imperdiet, venenatis et sapien. Duis sed magna
+            pulvinar, fringilla lorem eget, ullamcorper urna.
           </p>
         </div>
       </div>
@@ -25,8 +26,13 @@
 
     <!-- Main Content -->
     <section class="container mx-auto">
-      <div class="bg-white rounded border border-gray-200 relative flex flex-col">
-        <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200" v-icon>
+      <div
+        class="bg-white rounded border border-gray-200 relative flex flex-col"
+      >
+        <div
+          class="px-6 pt-6 pb-5 font-bold border-b border-gray-200"
+          v-icon="'headphones-alt'"
+        >
           <span class="card-title">Songs</span>
           <!-- Icon -->
         </div>
@@ -64,7 +70,8 @@ export default {
     hanldeScroll() {
       const { scrollTop, offsetHeight } = document.documentElement;
       const { innerHeight } = window;
-      const bottomOfWindow = Math.round(scrollTop) + innerHeight === offsetHeight;
+      const bottomOfWindow =
+        Math.round(scrollTop) + innerHeight === offsetHeight;
       if (bottomOfWindow) {
         this.getSongs();
       }
@@ -81,7 +88,9 @@ export default {
       let snapshots;
 
       if (this.songs.length) {
-        const lastDoc = await songsCollection.doc(this.songs[this.songs.length - 1].docID).get();
+        const lastDoc = await songsCollection
+          .doc(this.songs[this.songs.length - 1].docID)
+          .get();
 
         snapshots = await songsCollection
           .orderBy('modified_name')
@@ -89,7 +98,10 @@ export default {
           .limit(this.maxPerPage)
           .get();
       } else {
-        snapshots = await songsCollection.orderBy('modified_name').limit(this.maxPerPage).get();
+        snapshots = await songsCollection
+          .orderBy('modified_name')
+          .limit(this.maxPerPage)
+          .get();
       }
 
       snapshots.forEach((document) => {
