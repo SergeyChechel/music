5e33d42502da427940f4d4d4043b38b0baaed088
Sergey <chechelsk@gmail.com>
2023-11-10 16:30:58 +0200

    Exercise Translating the Rest of the App

--- src/views/Manage.vue
+++ src/views/Manage.vue
@@ -4,10 +4,14 @@
     <div class="md:grid md:grid-cols-3 md:gap-4">
       <app-upload ref="upload" :addSong="addSong" />
       <div class="col-span-2">
-        <div class="bg-white rounded border border-gray-200 relative flex flex-col">
+        <div
+          class="bg-white rounded border border-gray-200 relative flex flex-col"
+        >
           <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
-            <span class="card-title">My Songs</span>
-            <i class="fa fa-compact-disc float-right text-green-400 text-2xl"></i>
+            <span class="card-title">{{ $t('manage.my_songs') }}</span>
+            <i
+              class="fa fa-compact-disc float-right text-green-400 text-2xl"
+            ></i>
           </div>
           <div class="p-6">
             <!-- Composition Items -->
@@ -45,7 +49,9 @@ export default {
     };
   },
   async created() {
-    const snapshot = await songsCollection.where('uid', '==', auth.currentUser.uid).get();
+    const snapshot = await songsCollection
+      .where('uid', '==', auth.currentUser.uid)
+      .get();
 
     snapshot.forEach(this.addSong);
   },
@@ -71,7 +77,9 @@ export default {
   beforeRouteLeave(to, from, next) {
     if (!this.unsavedFlag) next();
     else {
-      const leave = confirm('You have unsaved changes. Are you sure you want to leave?');
+      const leave = confirm(
+        'You have unsaved changes. Are you sure you want to leave?'
+      );
       next(leave);
     }
   }
